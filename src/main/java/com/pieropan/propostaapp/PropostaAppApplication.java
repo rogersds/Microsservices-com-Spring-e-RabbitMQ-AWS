828c829714940269c6bf42850e91682531541fd4
rogersds <rogger_souza@hotmail.com>
2025-02-18 21:01:28 -0300

    Mapeando as entidades

--- src/main/java/com/pieropan/propostaapp/PropostaAppApplication.java
+++ src/main/java/com/pieropan/propostaapp/PropostaAppApplication.java
@@ -10,5 +10,5 @@ public class PropostaAppApplication {
 		SpringApplication.run(PropostaAppApplication.class, args);
 	}
 
-	
+
 }
---
+++ src/main/java/com/pieropan/propostaapp/entity/Proposta.java
@@ -0,0 +1,24 @@
+package com.pieropan.propostaapp.entity;
+
+import jakarta.persistence.*;
+import com.pieropan.propostaapp.entity.Usuario;
+
+@Entity
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valorSolicitado;
+    private int prazoPagamento;
+    private Boolean aprovada;
+    private boolean integrada;
+    private String observacao;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    // Getters, setters e outros métodos
+}
