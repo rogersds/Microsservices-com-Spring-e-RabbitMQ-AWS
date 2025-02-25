217019665b64655cd4ea834b3412214f37eacc6f
rogersds <rogger_souza@hotmail.com>
2025-02-25 16:17:33 -0300

    Criando Repository

--- src/main/java/com/pieropan/propostaapp/service/PropostaService.java
+++ src/main/java/com/pieropan/propostaapp/service/PropostaService.java
@@ -1,13 +1,21 @@
 package com.pieropan.propostaapp.service;
 
+import com.pieropan.propostaapp.Repository.PropostaRepository;
 import com.pieropan.propostaapp.dto.PropostaRequestDto;
 import com.pieropan.propostaapp.dto.PropostaResponseDto;
+import com.pieropan.propostaapp.entity.Proposta;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class PropostaService {
 
+    private PropostaRepository propostaRepository;
+
+
     public PropostaResponseDto criar(PropostaRequestDto requestDto){
+        propostaRepository.save(new Proposta());
         return null;
 
     }
